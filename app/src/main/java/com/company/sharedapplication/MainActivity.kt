32fe7a97fa934ef9cc3836784d0aaaead8d4450b
ridvan_mac <cakmak.ridvan@hotmail.com>
2019-10-02 17:47:24 +0300

    extension function added in Int class

--- app/src/main/java/com/company/sharedapplication/MainActivity.kt
+++ app/src/main/java/com/company/sharedapplication/MainActivity.kt
@@ -2,11 +2,24 @@ package com.company.sharedapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        txt_result.setText("Sonuc: " + 5.faktoriyelBul())
+    }
+
+    fun Int.faktoriyelBul() : Int{
+
+        var sonuc = 1
+
+        for (i in  1..this)
+            sonuc = sonuc * i
+
+        return sonuc
     }
 }
